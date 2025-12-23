35694b305fe85c1fd57055b32f8737cc9960acbd
OldWest Solutions <oldwest@oldwestsolutions.com>
2025-12-23 01:05:45 -0600

    Fix Header import error - Header should be destructured from Layout

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { Layout, Typography, Divider, Space, Button, Card, Input, Form, Checkbox, Header } from 'antd'
+import { Layout, Typography, Divider, Space, Button, Card, Input, Form, Checkbox } from 'antd'
 import Link from 'next/link'
 import { useState } from 'react'
 
-const { Content } = Layout
+const { Header, Content } = Layout
 const { Title, Paragraph, Text } = Typography
 
 export default function Account() {
